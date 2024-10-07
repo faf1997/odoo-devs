bcd7ac0cf80db43c332083a4b9e9e5237edcb8f0
faf1997 <alejandrofrafio@gmail.com>
2024-10-07 02:27:37 +0000

    [UPD] ahora el controller installe_modules solo devuelve modulos de terceros instalados

---
+++ client_code_tide/__manifest__.py
@@ -0,0 +1,11 @@
+{
+    "name": "Marea de código del cliente",
+    "author": "Francisco Fiorentino",
+    "category": "",
+    "summary": "API para el cliente que permite hacer seguimiento de los módulos instalados",
+    "version": "17.0.1.0.0",
+    "license": "AGPL-3",
+    "depends": [],
+    "data": [
+    ],
+}
